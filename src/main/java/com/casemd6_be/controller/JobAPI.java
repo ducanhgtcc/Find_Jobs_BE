3daa83f8f477d0b6bff9130b81960bab7ff42c6d
Tien Minh 08 <tanktan1212@gmail.com>
2023-03-10 17:40:31 +0700

    showNews

--- src/main/java/com/casemd6_be/controller/JobAPI.java
+++ src/main/java/com/casemd6_be/controller/JobAPI.java
@@ -1,15 +1,14 @@
 package com.casemd6_be.controller;
 
 import com.casemd6_be.model.Job;
+import com.casemd6_be.model.query.ListJobCompanyAccount;
 import com.casemd6_be.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.plaf.PanelUI;
 import java.util.List;
 
 @RestController
@@ -18,8 +17,21 @@ import java.util.List;
 public class JobAPI {
     @Autowired
     JobService jobService;
+
     @GetMapping("/showJob")
     public ResponseEntity<List<Job>> getAllJob() {
-       return new ResponseEntity<>(jobService.getAllJob(),HttpStatus.OK);
+        return new ResponseEntity<>(jobService.getAllJob(), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/showJobNew")
+    public ResponseEntity<List<ListJobCompanyAccount>>getAllJob_Latest(){
+       return new ResponseEntity<>(jobService.getAllJob_Latest(),HttpStatus.OK);
+
+    }
+
+    @GetMapping("/showJobNew/{id}")
+    public ResponseEntity<ListJobCompanyAccount> getOneJob(@PathVariable int id){
+        return new ResponseEntity<>(jobService.getOneJobbyID(id),HttpStatus.OK);
     }
 }
--- src/main/java/com/casemd6_be/service/JobService.java
+++ src/main/java/com/casemd6_be/service/JobService.java
@@ -1,17 +1,30 @@
 package com.casemd6_be.service;
 
 import com.casemd6_be.model.Job;
+import com.casemd6_be.model.query.ListJobCompanyAccount;
 import com.casemd6_be.repository.IJobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class JobService {
     @Autowired
     IJobRepo iJobRepo;
+
     public List<Job> getAllJob() {
         return (List<Job>) iJobRepo.findAll();
+    }
+
+    public List<ListJobCompanyAccount> getAllJob_Latest() {
+        List<ListJobCompanyAccount> ListJob_Latest =  iJobRepo.joinCompanyAndJobAndAccount();
+        Collections.reverse(ListJob_Latest);
+        return ListJob_Latest;
+    }
+    public ListJobCompanyAccount getOneJobbyID(int id) {
+       return iJobRepo.joinCompanyAndJobAndAccountbyid(id);
     }
 }
