ef7b823ce305928ac3e236cd4428b16f83a0a429
hungchivang <Phihung21111997@gmail.com>
2023-03-10 11:06:16 +0700

    sendMail

--- src/main/java/com/casemd6_be/controller/RegisterAPI.java
+++ src/main/java/com/casemd6_be/controller/RegisterAPI.java
@@ -3,6 +3,7 @@ package com.casemd6_be.controller;
 import com.casemd6_be.model.Account;
 import com.casemd6_be.model.Role;
 import com.casemd6_be.service.AccountService;
+import com.casemd6_be.service.SendEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,9 @@ public class RegisterAPI {
 
     @Autowired
     AccountService accountService;
+
+    @Autowired
+    SendEmailService sendEmailService;
 
     @GetMapping
     public ResponseEntity<List<Account>> findAll() {
@@ -33,10 +37,12 @@ public class RegisterAPI {
             account.setRole(role);
             account.setAvatar("https://i.pinimg.com/236x/16/b2/e2/16b2e2579118bf6fba3b56523583117f.jpg");
             account.setStatus(false);
+
+            sendEmailService.sendMail(account.getEmail(), "Thông báo","Tài khoản "+ account.getName()+ " đã được đăng kí." +
+                    "Xin chờ hệ thống xác nhân từ 1 đến 2 ngày");
             accountService.save(account);
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
 }
